100b5c4ccde6708e1ef38b1be97fd1a6e9205c74
jomarmontuya <jomar.montuya@outlook.com>
2020-05-19 13:58:33 +0800

    Initial Commit Covid Landing page

---
+++ precache-manifest.cdf3b79c1f15d1826a9c61d35658b27a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b736a80d52bab09c2be5016d2758a8c1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ed7a46be87d845f2d124",
+    "url": "./static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "eec61dfb34c1f9923522",
+    "url": "./static/js/2.5fce7b7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5fce7b7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7a46be87d845f2d124",
+    "url": "./static/js/main.74b0ac73.chunk.js"
+  },
+  {
+    "revision": "f6d2e74b04eafbc473cb",
+    "url": "./static/js/runtime-main.c257f219.js"
+  }
+]);
